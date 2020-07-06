82f2b7a219a4dc33a7be4e484add7b6529ba628f
beholdShadow <528540982@qq.com>
2020-07-06 18:36:29 +0800

    7.6 18:36

--- SSDP/test1.c
+++ SSDP/test1.c
@@ -27,33 +27,29 @@ int main(int argc,char *argv[])
 		perror("socket failed!");
 		return 2;
 	}
-                int opt = 1;
-                 if(setsockopt(socked, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt, sizeof(opt)) < 0)
-                {
-                                perror("fail to setsockopt");
-                                return -1;
-                }
+
 	struct sockaddr_in remote_addr;
 	memset(&remote_addr,0,sizeof(remote_addr));
 	
                bzero(&remote_addr, sizeof(remote_addr));  
                 remote_addr.sin_family = AF_INET;  
                 remote_addr.sin_port = htons(8888);
-                remote_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-                if(bind(socked, (struct sockaddr *)&remote_addr, sizeof(remote_addr))<0) 
-                 {
-                                 perror("ssdp_sock bind"); 
-                                 return -1;
-                } 
-                struct ip_mreq mreq;
-                mreq.imr_multiaddr.s_addr = inet_addr("224.0.0.0");
-                mreq.imr_interface.s_addr =htonl(INADDR_ANY);
+                remote_addr.sin_addr.s_addr = inet_addr("224.0.0.88");
+                //htonl(INADDR_ANY);
+                // if(bind(socked, (struct sockaddr *)&remote_addr, sizeof(remote_addr))<0) 
+                //  {
+                //                  perror("ssdp_sock bind"); 
+                //                  return -1;
+                // } 
+                // struct ip_mreq mreq;
+                // mreq.imr_multiaddr.s_addr = inet_addr("224.0.0.88");
+                // mreq.imr_interface.s_addr =htonl(INADDR_ANY);
 
-                if(setsockopt(socked, IPPROTO_IP,IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
-                {
-                                perror("fail to setsockopt");
-                                return -1;
-                }
+                // if(setsockopt(socked, IPPROTO_IP,IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
+                // {
+                //                 perror("fail to setsockopt");
+                //                 return -1;
+                // }
 
 
 	char buf[1024]="This is a group udp";
@@ -68,13 +64,13 @@ int main(int argc,char *argv[])
 	// close(socked);
 
                 // struct  Device_Msg tmp={Discovery,"",""};
-                // memcpy(buf,&tmp,sizeof(struct Device_Msg));
-                // strcpy(buf,"hello world");
-                // length=sendto(socked,buf,strlen(buf),0,(struct sockaddr *)&remote_addr,sizeof(remote_addr));
-                // memset(buf, 0, sizeof(buf));  
-                if(recvfrom(socked,buf,sizeof(buf),0,NULL,NULL) >=0)
-                                printf("receive data from server\n");
-                printf("buf=%s\n",buf);
+                //memcpy(buf,&tmp,sizeof(struct Device_Msg));
+                strcpy(buf,"hello world");
+                length=sendto(socked,buf,strlen(buf),0,(struct sockaddr *)&remote_addr,sizeof(remote_addr));
+                memset(buf, 0, sizeof(buf));  
+                // if(recvfrom(socked,buf,sizeof(buf),0,NULL,NULL) >=0)
+                //                 printf("receive data from server\n");
+                // printf("buf=%s\n",buf);
 
 	return 0;
 }
